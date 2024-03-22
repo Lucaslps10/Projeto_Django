6906f276fa912326edadac4bc259cb45284a7d2c
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2024-03-22 19:41:17 +0000

    Edicao do meu projeto

--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pesquisar(request):
+    print("Opa chegou aqui")
+
+def cadastrar(request):
+    print("Chegou no cadastrar")
